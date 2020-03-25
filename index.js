f5957cc6e95c3d7c49b8f773ce8ea013ef909c87
Psalmuel-codes <62013243+Psalmuel-codes@users.noreply.github.com>
2020-03-25 15:27:05 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+//store your name in variable
+let name="_psalmuel";
+console.log("My name is:" + name);
+
+//store your course in an array
+const course=[];
+course.push("Frontend");
+console.log(course);
+
+//if the number of the courses you is an odd number,find and display all odd numbers from 1-200
+for (let i=0; i<200; i++) {
+    if(i%2!==0)
+    console.log(i);
+}
+
